8368d67c7eb64bdebc8fcb9b27fd865c9f200117
Cleiton de Lima <CleitonDeLima@users.noreply.github.com>
2020-02-29 19:19:38 -0300

    Version 0.5.0 (#17)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-login-required-middleware',
-    version='0.4',
+    version='0.5.0',
     packages=[
         'login_required',
     ],
