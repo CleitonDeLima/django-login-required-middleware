30eeda965f7f2cbad83c7e881148fad025184c18
Cleiton Lima <cleiton.limapin@gmail.com>
2020-06-13 15:36:08 -0300

    Added optional decorator/mixin view

---
+++ login_required/__init__.py
@@ -0,0 +1,18 @@
+default_app_config = 'login_required.apps.LoginRequiredConfig'
+
+
+def login_not_required(view_func):
+    """
+    Decorator for views that marks that the view is accessible by
+    unauthenticated users.
+    """
+    view_func.login_required = False
+    return view_func
+
+
+class LoginNotRequiredMixin:
+    """
+    Mixin for CBV that marks that the view is accessible by
+    unauthenticated users.
+    """
+    login_required = False
