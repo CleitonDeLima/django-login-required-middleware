673d1c88bef79431f20fdea9e52269333f486334
Cleiton de Lima <CleitonDeLima@users.noreply.github.com>
2020-06-13 14:21:08 -0300

    Dropped support for Django 1.11 (#24)

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 from django.http import HttpResponse
 
 urlpatterns_with_namespace = [
-    url(r'^foo2/$', lambda request: HttpResponse('foo2'), name='foo')
+    path('foo2/', lambda request: HttpResponse('foo2'), name='foo')
 ]
 
 urlpatterns = [
-    url(r'^bar/$', lambda request: HttpResponse('bar'), name='bar'),
-    url(r'^foo/$', lambda request: HttpResponse('foo'), name='foo'),
-    url(r'^admin/', admin.site.urls),
-    url('', include((urlpatterns_with_namespace, 'app')))
+    path('bar/', lambda request: HttpResponse('bar'), name='bar'),
+    path('foo/', lambda request: HttpResponse('foo'), name='foo'),
+    path('admin/', admin.site.urls),
+    path('', include((urlpatterns_with_namespace, 'app'))),
 ]
