13382c19652e8d74ab277854d31c4b10e5070ece
Cleiton Lima <cleiton.limapin@gmail.com>
2021-09-14 08:47:27 -0300

    Release 0.6.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-login-required-middleware",
-    version="0.6.1",
+    version="0.6.2",
     packages=[
         "login_required",
     ],
